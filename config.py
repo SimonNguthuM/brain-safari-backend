7d98afc5b764c02532151551512913db445d2290
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-11-17 14:30:29 +0300

    render

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv() 
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_COOKIE_SAMESITE = "None"  
     SESSION_COOKIE_SECURE = True    
 
-    STATIC_FOLDER = os.path.join(os.getcwd(), 'build')
+    STATIC_FOLDER = os.path.join(os.getcwd(), 'frontend/build')
