29609f819b565e91982dc19aa97dbf9091fe4047
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-11-18 17:06:00 +0300

    .

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def signup():
         username = data.get('username')
         email = data.get('email')
         password = data.get('password')
-        role = data.get('role', "Contributor")  
+        role = data.get('role', "Learner")  
 
         if not username or not email or not password:
             return jsonify({"error": "All fields are required"}), 400
